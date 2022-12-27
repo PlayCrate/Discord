923edac20e85cda16d6033394cdbd0aca456563d
Kattah <kattahpro@gmail.com>
2022-12-28 07:30:27 +0900

    Fixed build

--- src/events/modules/Commands.ts
+++ src/events/modules/Commands.ts
@@ -2,9 +2,9 @@ import { readdirSync } from "fs";
 const commands = new Map();
 
 export const loadCommands = (dir = "./src/commands/") => {
-     for (let file of readdirSync(dir).filter((f) => f.endsWith(".ts"))) {
-          let pull = require(`../../commands/${file}`);
-          if (pull.name) {
+     for (let file of readdirSync("./").filter((f) => f.endsWith(".ts"))) {
+          let pull = require("../../commands/" + file);
+          if (pull?.name) {
                commands.set(pull.name, pull);
           }
      }
