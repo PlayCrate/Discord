6125b479743ad9b59a444d5ddb429a7779cae4d7
Kattah <kattahpro@gmail.com>
2022-12-28 13:33:15 +0900

    Fixed Compiler

--- src/events/modules/Commands.ts
+++ src/events/modules/Commands.ts
@@ -1,11 +1,14 @@
-import { readdirSync } from "fs";
+import * as fs from "fs";
 import { type } from "../../../config.json";
 const commands = new Map();
 
-export const loadCommands = (dir = "./src/commands/") => {
-     for (let file of readdirSync(dir).filter((f) =>
-          f.endsWith(type === "prod" ? ".js" : ".ts")
-     )) {
+export const loadCommands = () => {
+     const typeSync =
+          type === "dev" ? "./src/commands/" : "./build/src/commands/";
+     const typeCompiler = type === "dev" ? ".ts" : ".js";
+     for (let file of fs
+          .readdirSync(typeSync)
+          .filter((f) => f.endsWith(typeCompiler))) {
           let pull = require(`../../commands/${file}`);
           if (pull.name) {
                commands.set(pull.name, pull);
